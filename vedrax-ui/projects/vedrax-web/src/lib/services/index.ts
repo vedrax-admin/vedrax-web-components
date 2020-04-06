27d40afe218c6f172be90367c61887d21abdb992
penchenatremy <finance@vedrax.com>
2020-04-06 14:19:51 +0300

    update index in service

--- vedrax-ui/projects/vedrax-web/src/lib/services/index.ts
+++ vedrax-ui/projects/vedrax-web/src/lib/services/index.ts
@@ -1,6 +1,7 @@
 export * from './authentication.service';
 export * from './dialog-form.service';
 export * from './form.service';
+export * from './loader.service';
 export * from './url.service';
 export * from './vedrax-api.service';
 export * from './vedrax-data.service';
