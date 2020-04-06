6d45dc22c2fc872b0aa321e819818d9091d05408
penchenatremy <finance@vedrax.com>
2020-04-06 14:17:17 +0300

    update index for enum

--- vedrax-ui/projects/vedrax-web/src/lib/shared/index.ts
+++ vedrax-ui/projects/vedrax-web/src/lib/shared/index.ts
@@ -1,3 +1,6 @@
 export * from './matrixColumn';
 export * from './nvp';
 export * from './vedrax-base.component';
+export * from './role.enum';
+export * from './user-dto';
+export * from './user.model';
