111dbaa2b4b74598b7fbf082086afbd8eeaf632c
Remy Penchenat <finance@vedrax.com>
2021-03-01 07:19:47 +0200

    add type to index

--- vedrax-ui/projects/vedrax-web/src/lib/enum/index.ts
+++ vedrax-ui/projects/vedrax-web/src/lib/enum/index.ts
@@ -1,5 +1,6 @@
 export * from './action-types';
 export * from './api-methods';
+export * from './comparator-type';
 export * from './control-types';
 export * from './input-types';
 export * from './msg-level';
